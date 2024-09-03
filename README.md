d9d4c99741f4a96231356ba33877f5485a6a95d4
yonekura-k <k.yonekura716@gmail.com>
2024-09-03 17:14:36 +0900

    todoアプリ作成

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
-# Writer Framework_todo
+# Writer Framework todo
 Writer FrameworkでToDoアプリを作る
 
 ## 開発環境準備
 Writer Frameworkのバージョンは0.7.2を使用。
+
 [公式](https://dev.writer.com/framework/quickstart)より、Pythonのバージョンは3.9.2以上を使うこと。
+
 動かしたrequirements.txtで出力しているが、以下のpipで動く想定。
+
 ```
 pip install writer==0.7.2
 ```
 
 ## 実行方法
+```
 writer run todo_app
+```
+
+起動後、以下にアクセス。
+
+http://127.0.0.1:3006
+
+## 補足
+以下のブログにて解説
+
+https://tech-useit-wealth.com/writer-framework-todo
+
--- todo_app/main.py
+++ todo_app/main.py
@@ -1,14 +1,45 @@
 import writer as wf
 import pandas as pd
 
+
+def get_date(state, payload):
+    state["get_date"] = payload
+
+
+def get_task(state, payload):
+    state["get_task"] = payload
+
+
+def add_df_loc(state):
+    if state["get_date"] == "" or state["get_task"] == "":
+        state.add_notification(
+            "warning",
+            "追加情報に記入漏れがあります.",
+            "日付とタスクを入力してください.",
+        )
+        return
+    state["df"] = pd.concat(
+        [
+            state["df"],
+            pd.DataFrame({"日付": [state["get_date"]], "タスク": [state["get_task"]]}),
+        ]
+    )
+
+
+def delete_df_loc(state):
+    state["df"] = state["df"].iloc[:-1]
+
+
 initial_state = wf.init_state(
     {
         "my_app": {"title": "MY APP"},
         "df": pd.DataFrame(
             {
-                "日付": ["2024-9-1", "2024-9-1", "2024-9-2"],
+                "日付": ["2024-9-1", "2024-9-2", "2024-9-3"],
                 "タスク": ["買い物", "掃除", "勉強"],
             },
         ),
+        "get_date": "",
+        "get_task": "",
     }
 )
